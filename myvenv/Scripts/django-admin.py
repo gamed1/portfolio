62b887eb13e09cedabb7fc57dcda2d200994e336
edwin <edwin@van-eck.net>
2018-05-14 11:05:49 +0200

    add jobs and blog apps

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\edwin\google drive\programming\django\projects\portfolio-proj\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
